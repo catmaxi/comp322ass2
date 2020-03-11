1ec7881832477e4ffc07d6a50897d6fccad34ceb
wenxuan27 <wenxuan27@outlook.com>
2020-03-11 00:15:53 -0400

    added all the methods to the classes but not completed

--- blackjack.cpp
+++ blackjack.cpp
@@ -5,6 +5,7 @@
 #include "blackjack.h"
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -40,35 +41,114 @@ private:
   Type type;
 
 public:
+  Card()
+  {
+  }
+
   int getValue()
   {
     return this->rank;
   }
+
   void displayCard()
   {
-    cout << getValue() << endl;
+    char C;
+    switch (this->type)
+    {
+    case CLUBS:
+      C = 'C';
+      break;
+    case DIAMONDS:
+      C = 'D';
+      break;
+    case HEARTS:
+      C = 'H';
+      break;
+    case SPADES:
+      C = 'S';
+      break;
+    }
+
+    cout << getValue() << C << endl;
   }
 };
 
 class Hand
 {
+private:
+  vector<int> cards;
+
+public:
+  Hand()
+  {
+  }
+  void add()
+  {
+  }
+  void clear()
+  {
+  }
+  int getTotal()
+  {
+  }
 };
 
 class Deck
 {
+private:
+  vector<int> cards;
+
+public:
+  Deck()
+  {
+  }
+  void Populate()
+  {
+  }
+  void shuffle()
+  {
+  }
+  void deal()
+  {
+  }
 };
 
 class AbstractPlayer
 {
+public:
+  virtual bool isDrawing() const = 0;
+  bool isBusted()
+  {
+  }
 };
 
-class Player
+class HumanPlayer : public AbstractPlayer
 {
+private:
+public:
+  virtual bool isDrawing()
+  {
+  }
+  void announce()
+  {
+  }
 };
-class Computer
+
+class ComputerPlayer : public AbstractPlayer
 {
+private:
+public:
+  virtual bool isDrawing()
+  {
+    return false;
+  }
 };
 
 class BlackJackGame
 {
+private:
+  Deck m_deck;
+  ComputerPlayer m_casino;
+
+public:
 };
